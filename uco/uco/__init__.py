3504ab206e0cb4518317cecc77ceddfcf62b9b12
Dylan Hyun <dchyun@andrew.cmu.edu>
2018-03-25 15:50:24 -0400

    changed scraping file to export a template

--- uco/uco/__init__.py
+++ uco/uco/__init__.py
@@ -13,7 +13,7 @@ def main():
 @app.route('/compare', methods=['GET', 'POST'])
 def compare(): 
 	message = None
-	os.system('scrapy runspider uco/asana_test.py')
+	os.system('scrapy runspider asana_test.py')
 
 	# if request.method == 'POST':
 	# 	datafromjs = request.form['mydata']
@@ -21,7 +21,7 @@ def compare():
 	# 	resp = make_response('{"response": '+result+'}')
 	# 	resp.headers['Content-Type'] = "application/json"
 	# 	return resp
-	return render_template('index.html', message='')
+	return render_template('changes.html', message='')
 
 if __name__ == '__main__':
     app.run()
--- uco/uco/asana_test.py
+++ uco/uco/asana_test.py
@@ -1,36 +1,32 @@
 # run this file as
 #
-# scrapy runspider asana_test.py
+# scrapy runspider asana.py
 
 from scrapy import Request
 from scrapy.spiders import Spider
 
-# Spider definition
+from difflib import Differ
+from pprint import pprint
+
 class S1(Spider):
     name = 's1'
     custom_settings = {
             'DOWNLOAD_DELAY': 0.5
             }
     allowed_domains = ['asana.com']
-    # this would have to be updated for each software
     start_urls = ["https://asana.com/terms"]
 
-    # parse automatically gets called when the file is run
-    def parse(self, response): 
-        # use xpath to get a list of all the text in the page
+    def parse(self, response):
         text = response.xpath("//body//text()").extract()
         text = ''.join(text)
         text = clean_text(text,"Asana User Terms of Service", "an integral link.")
-        # write terms into a txt file
         f= open("asana.txt","w+")
         for line in text:
             f.write(str(line))
-        f.close() 
-        return text
+        f.close()
+        compare("asana_old.txt","asana.txt","templates/changes_table.html")
 
-
-# remove all text before start and after end
-# start and end would have to be sent in by the user
+# takes away all text before start and after end
 def clean_text(s,start,end):
     ans = s
     i = s.find(start)
@@ -44,3 +40,59 @@ def clean_text(s,start,end):
     else:
         ans = s
     return ans
+
+# compares a and b and outputs a HTML table of the changes. 
+
+def compare(a,b,filename):
+    out = get_differences(a,b)
+    make_html_table(out,filename)
+
+# using the Differ module of the difflib library to compare 
+# outputs a generator object that is converted into a list of 
+# the changes of the form [[added/removed, line]]
+# - -> line was removed
+# + -> line was added
+def get_differences(a,b):
+    a_list = split_txt(a)
+    b_list = split_txt(b)
+    d = Differ()
+    result = list(d.compare(a_list,b_list))
+    for i in range(0,len(result)):
+        result[i]=result[i].split()
+    return result
+
+def split_txt(txt):
+    f = open(txt,"r+")
+    ans = f.read().splitlines()
+    f.close()
+    return ans
+
+# created an html table that assigns a class to the rows
+# that have been added and removed
+def make_html_table(l,name):
+    ans = ['<table>\n']
+    for line in l:
+        front = '\t<tr>\n\t\t<td>'
+        if len(line) == 0 :
+            continue
+        elif line[0] == "?":
+            continue
+        elif line[0] == "-":
+            front = '\t<tr>\n\t\t<td class ='+'"removed">'
+        elif line[0] == "+":
+            front = '\t<tr>\n\t\t<td class ='+'"added">'
+        ans.append(str(add_line(line,front)))
+    ans.append('</table>')
+    write_file(ans,name)
+
+def add_line(line,front):
+    end = '</td>\n\t</tr>\n'
+    if (line[0] == "+" or line[0] == "-"):
+        line = line[1:]
+    return front+" ".join(line)+end
+
+def write_file(l,name):
+    f = open(name,"w+")
+    for line in l:
+        f.write(str(line))
+    f.close()
